38c394e8332c3de32c577b6c3c1e92b115b25bb9
Reggie Burnett <reggie.burnett@oracle.com>
2013-03-26 15:45:44 -0500

    implemented timer class

---
+++ Source/MySql.Data/base/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.System.Threading;
+using Windows.UI.Xaml;
+
+namespace MySql.Data
+{
+    internal class Timer
+    {
+      TimeSpan period;
+      TimerCallback handler;
+      private int startDelay;
+
+      public Timer(TimerCallback callback, object state, Int32 dueTime, Int32 periodInSeconds)
+      {
+        period = new TimeSpan(0, 0, periodInSeconds);
+        handler = callback;
+        startDelay = dueTime;
+        ThreadPoolTimer tpt = ThreadPoolTimer.CreatePeriodicTimer(new TimerElapsedHandler(Elapsed), period);
+      }
+
+      private void Elapsed(ThreadPoolTimer timer)
+      {
+        if (startDelay > 0)
+        {
+          startDelay -= (int)period.TotalSeconds;
+          if (startDelay > 0) return;
+        }
+
+        if (handler != null)
+          handler(null);
+      }
+    }
+
+    public delegate void TimerCallback(Object state);
+}
