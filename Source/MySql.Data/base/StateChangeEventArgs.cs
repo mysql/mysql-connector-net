939b5532d1098acb3ceb353b4692047b2c1b4213
Roberto E. Garcia <robertoe.garcia@oracle.com>
2013-04-05 15:16:37 -0600

    Added new files and solution file.

---
+++ Source/MySql.Data/base/StateChangeEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySql.Data.MySqlClient
+{
+  public class StateChangeEventArgs : EventArgs
+    {
+      public StateChangeEventArgs(ConnectionState originalState, ConnectionState currentState)
+      {
+        CurrentState = currentState;
+        OriginalState = originalState;
+      }
+
+      public ConnectionState CurrentState { get; private set; }
+
+      public ConnectionState OriginalState { get; private set; }
+    }
+}
---
+++ Source/MySql.Data/extensions/RT/ArrayList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Collections
+{
+  public class ArrayList : List<object>
+  {
+    public Array ToArray(Type type)
+    {
+      Array tempArray = Array.CreateInstance(type);
+      Array.Copy(this.ToArray(), tempArray, this.Count);
+
+      return tempArray;
+    }
+  }
+}
