0f7336c434c086646acd2e4ec0df908e1c8b5836
Fernando Gonzalez Sanchez <fer.gonzalez@oracle.com>
2013-05-13 09:38:03 -0500

    Fix for "Cannot change the name for a Foreign Key in table designer" (Oracle bug #16238068). - Now typing a name in FK editor, does not search for that FK in the list, just accept it as its new name. - FKs are saved using the name for the constraint (instead of the foreign key index name).

--- Source/MySql.VisualStudio/Editors/ForeignKeyDialog.cs
+++ Source/MySql.VisualStudio/Editors/ForeignKeyDialog.cs
@@ -1,4 +1,4 @@
-// Copyright © 2008, 2010, Oracle and/or its affiliates. All rights reserved.
+// Copyright © 2008, 2013, Oracle and/or its affiliates. All rights reserved.
 //
 // MySQL Connector/NET is licensed under the terms of the GPLv2
 // <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
@@ -376,11 +376,7 @@ namespace MySql.Data.VisualStudio.Editors
 
     private void fkName_TextChanged(object sender, EventArgs e)
     {
-      if (fkName.Modified)
-      {
-        ForeignKey key = foreignKeyBindingSource.Current as ForeignKey;
-        key.NameSet = true;
-      }
+      (foreignKeyBindingSource.Current as ForeignKey).Name = fkName.Text;
     }
 
   }
