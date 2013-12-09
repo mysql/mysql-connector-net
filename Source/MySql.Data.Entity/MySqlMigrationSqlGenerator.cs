45c04b3022bab24e687512be6bb96899419ac718
Francisco Tirado <francisco.tirado@oracle.com>
2013-12-09 17:32:33 -0600

    Bug MySql 70795: Entity Framework migration with Foreign Key fails. Fix: Remove ".dbo" schema from the mysql script generation.

--- Source/MySql.Data.Entity/MySqlMigrationSqlGenerator.cs
+++ Source/MySql.Data.Entity/MySqlMigrationSqlGenerator.cs
@@ -276,11 +276,11 @@ namespace MySql.Data.Entity
     {
 
       StringBuilder sb = new StringBuilder();
-      sb.Append("alter table `" + op.DependentTable + "` add constraint `" + op.Name + "` " +
+      sb.Append("alter table `" + TrimSchemaPrefix(op.DependentTable) + "` add constraint `" + TrimSchemaPrefix(op.Name) + "` " +
                  " foreign key ");
 
       sb.Append("(" + string.Join(",", op.DependentColumns.Select(c => "`" + c + "`")) + ") ");
-      sb.Append("references `" + op.PrincipalTable + "` ( " + string.Join(",", op.PrincipalColumns.Select(c => "`" + c + "`")) + ") ");
+      sb.Append("references `" + TrimSchemaPrefix(op.PrincipalTable) + "` ( " + string.Join(",", op.PrincipalColumns.Select(c => "`" + c + "`")) + ") ");
 
       if (op.CascadeDelete)
       {
@@ -342,13 +342,13 @@ namespace MySql.Data.Entity
         if (op.IsIdentity && String.Compare(type, "CHAR(36) BINARY", true) == 0)
         {
           var createTrigger = new StringBuilder();
-          createTrigger.AppendLine(string.Format("DROP TRIGGER IF EXISTS `{0}_IdentityTgr`;", _tableName));
-          createTrigger.AppendLine(string.Format("CREATE TRIGGER `{0}_IdentityTgr` BEFORE INSERT ON `{0}`", _tableName));
+          createTrigger.AppendLine(string.Format("DROP TRIGGER IF EXISTS `{0}_IdentityTgr`;", TrimSchemaPrefix(_tableName)));
+          createTrigger.AppendLine(string.Format("CREATE TRIGGER `{0}_IdentityTgr` BEFORE INSERT ON `{0}`", TrimSchemaPrefix(_tableName)));
           createTrigger.AppendLine("FOR EACH ROW BEGIN");
           createTrigger.AppendLine(string.Format("SET NEW.{0} = UUID();", op.Name));
-          createTrigger.AppendLine(string.Format("DROP TEMPORARY TABLE IF EXISTS tmpIdentity_{0};", _tableName));
-          createTrigger.AppendLine(string.Format("CREATE TEMPORARY TABLE tmpIdentity_{0} (guid CHAR(36))ENGINE=MEMORY;", _tableName));
-          createTrigger.AppendLine(string.Format("INSERT INTO tmpIdentity_{0} VALUES(New.{1});", _tableName, op.Name));
+          createTrigger.AppendLine(string.Format("DROP TEMPORARY TABLE IF EXISTS tmpIdentity_{0};", TrimSchemaPrefix(_tableName)));
+          createTrigger.AppendLine(string.Format("CREATE TEMPORARY TABLE tmpIdentity_{0} (guid CHAR(36))ENGINE=MEMORY;", TrimSchemaPrefix(_tableName)));
+          createTrigger.AppendLine(string.Format("INSERT INTO tmpIdentity_{0} VALUES(New.{1});", TrimSchemaPrefix(_tableName), op.Name));
           createTrigger.AppendLine("END");
           var sqlOp = new SqlOperation(createTrigger.ToString());
           _specialStmts.Add(Generate(sqlOp));
