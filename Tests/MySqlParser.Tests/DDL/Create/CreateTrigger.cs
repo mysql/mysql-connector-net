5b582ed51e22b249a82ff4df080510a988f10cb2
Fernando Gonzalez Sanchez <fer.gonzalez@oracle.com>
2014-04-28 14:42:43 -0500

    Implementation of WI #1115, new syntax in 5.7 for adding multiple triggers for a table.

--- Tests/MySqlParser.Tests/DDL/Create/CreateTrigger.cs
+++ Tests/MySqlParser.Tests/DDL/Create/CreateTrigger.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2013 Oracle and/or its affiliates. All rights reserved.
+﻿// Copyright © 2014 Oracle and/or its affiliates. All rights reserved.
 //
 // MySQL Connector/NET is licensed under the terms of the GPLv2
 // <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
@@ -115,6 +115,61 @@ END IF;
 END;");
     }
 
+    [Fact]
+    public void FollowsTriggerSyntax56()
+    {
+      StringBuilder sb;
+      MySQL51Parser.program_return r = Utility.ParseSql(@"CREATE TRIGGER sanityCheck
+BEFORE INSERT ON my_table
+FOR EACH ROW FOLLOWS mytrigger
+BEGIN
+IF something THEN
+SET NEW.S_ID = 0 ;
+END IF;
+END;", true, out sb, new Version( 5, 6 ) );
+    }
+
+    [Fact]
+    public void FollowsTriggerSyntax57()
+    {
+      StringBuilder sb;
+      MySQL51Parser.program_return r = Utility.ParseSql(@"CREATE TRIGGER sanityCheck
+BEFORE INSERT ON my_table
+FOR EACH ROW FOLLOWS mytrigger
+BEGIN
+IF something THEN
+SET NEW.S_ID = 0 ;
+END IF;
+END;", false, out sb, new Version(5, 7));
+    }
+
+    [Fact]
+    public void PrecedesTriggerSyntax56()
+    {
+      StringBuilder sb;
+      MySQL51Parser.program_return r = Utility.ParseSql(@"CREATE TRIGGER sanityCheck
+BEFORE INSERT ON my_table
+FOR EACH ROW PRECEDES mytrigger
+BEGIN
+IF something THEN
+SET NEW.S_ID = 0 ;
+END IF;
+END;", true, out sb, new Version(5, 6));
+    }
+
+    [Fact]
+    public void PrecedesTriggerSyntax57()
+    {
+      StringBuilder sb;
+      MySQL51Parser.program_return r = Utility.ParseSql(@"CREATE TRIGGER sanityCheck
+BEFORE INSERT ON my_table
+FOR EACH ROW PRECEDES mytrigger
+BEGIN
+IF something THEN
+SET NEW.S_ID = 0 ;
+END IF;
+END;", false, out sb, new Version(5, 7));
+    }
 //    [Fact]
 //    public void ForEachRow3()
 //    {
