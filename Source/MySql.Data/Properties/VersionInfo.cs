d0a0b157da2ea9d1314f36b3debdc5bc10028d1a
robertoe_garcia <robertoe.garcia@oracle.com>
2014-05-06 08:10:17 -0500

    Bump up version to 6.6.7

--- Source/MySql.Data/Properties/VersionInfo.cs
+++ Source/MySql.Data/Properties/VersionInfo.cs
@@ -37,5 +37,5 @@ using System.Security;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("6.6.6")]
+[assembly: AssemblyVersion("6.6.7")]
 
