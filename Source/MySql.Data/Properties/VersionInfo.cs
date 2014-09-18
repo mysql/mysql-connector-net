e9e96f67c53ab2c5077c1fcd27a9d26edc2bc295
Roberto E. Garcia <robertoe.garcia@oracle.com>
2014-09-18 18:33:36 -0500

    Bump up version to 6.9.4 Added MySql.Fabric.Plugin to Installer.

--- Source/MySql.Data/Properties/VersionInfo.cs
+++ Source/MySql.Data/Properties/VersionInfo.cs
@@ -38,6 +38,6 @@ using System.Resources;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("6.9.3")]
-[assembly: AssemblyInformationalVersion("6.9.3")]
+[assembly: AssemblyVersion("6.9.4")]
+[assembly: AssemblyInformationalVersion("6.9.4")]
 [assembly: NeutralResourcesLanguage("en-US")]
