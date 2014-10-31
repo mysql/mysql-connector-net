517aca0c5a6c04ee1409787c75a6de808ca412e8
Francisco Tirado <francisco.tirado@oracle.com>
2014-10-31 15:21:53 -0600

    Memory leak in a loop opening a connection to the database and executing a command (MySql Bug #73122, Oracle Bug #19467233). Fix: add a destructor to properly call Dispose when the object is not used anymore.

--- Source/MySql.Data/Connection.cs
+++ Source/MySql.Data/Connection.cs
@@ -1,4 +1,4 @@
-// Copyright © 2004, 2013, Oracle and/or its affiliates. All rights reserved.
+// Copyright © 2004, 2014, Oracle and/or its affiliates. All rights reserved.
 //
 // MySQL Connector/NET is licensed under the terms of the GPLv2
 // <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
@@ -84,6 +84,13 @@ namespace MySql.Data.MySqlClient
     {
       ConnectionString = connectionString;
     }
+
+    #region Destructor
+    ~MySqlConnection()
+    {
+      Dispose(false);
+    }
+    #endregion
 
     #region Interal Methods & Properties
 
@@ -801,8 +808,8 @@ namespace MySql.Data.MySqlClient
 
     public void Dispose()
     {
-      if (State == ConnectionState.Open)
-        Close();
+      Dispose(true);
+      GC.SuppressFinalize(this);
     }
   }
 
