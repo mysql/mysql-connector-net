c555df44be0132791856674f42f6da3c08909a3f
Roberto E. Garcia <robertoe.garcia@oracle.com>
2014-11-03 11:27:35 -0600

    Bump up version to 6.9.5

--- Source/MySql.Data/Properties/VersionInfo.cs
+++ Source/MySql.Data/Properties/VersionInfo.cs
@@ -38,6 +38,6 @@ using System.Resources;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("6.9.4")]
-[assembly: AssemblyInformationalVersion("6.9.4")]
+[assembly: AssemblyVersion("6.9.5")]
+[assembly: AssemblyInformationalVersion("6.9.5")]
 [assembly: NeutralResourcesLanguage("en-US")]
