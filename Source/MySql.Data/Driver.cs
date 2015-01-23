8f0c32e5bb0c9fb91fd00a3f35a5b2b2b9f690cd
Roberto E. Garcia <robertoe.garcia@oracle.com>
2015-01-23 15:23:17 -0600

    Fix for chinese characters used in the connection string when connecting to MySql Server (MySQL Bug #70041, Oracle Bug #18141356).
    By default the character set used in handshake and authentication will be UTF-8 in order to support extended characters as chinese.

--- Source/MySql.Data/Driver.cs
+++ Source/MySql.Data/Driver.cs
@@ -1,4 +1,4 @@
-// Copyright © 2004, 2013, 2015, Oracle and/or its affiliates. All rights reserved.
+// Copyright © 2004, 2015, Oracle and/or its affiliates. All rights reserved.
 //
 // MySQL Connector/NET is licensed under the terms of the GPLv2
 // <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
@@ -72,7 +72,7 @@ namespace MySql.Data.MySqlClient
 
     public Driver(MySqlConnectionStringBuilder settings)
     {
-      encoding = Encoding.GetEncoding("Windows-1252");
+      encoding = Encoding.GetEncoding("UTF-8");
       if (encoding == null)
         throw new MySqlException(Resources.DefaultEncodingNotFound);
       connectionString = settings;
