3ba3994400c90473ffeae593a226dadd03fd34e2
Gabriela Martinez <gabry.martinez@oracle.com>
2016-03-04 17:48:43 -0600

    Updated README, AssemblyInfo and VersionInfo for 7.0

--- Source/MySql.Data/Properties/AssemblyInfo.cs
+++ Source/MySql.Data/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-// Copyright © 2004, 2015, Oracle and/or its affiliates.  All rights reserved.
+// Copyright © 2004, 2016, Oracle and/or its affiliates.  All rights reserved.
 //
 // MySQL Connector/NET is licensed under the terms of the GPLv2
 // <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
@@ -35,7 +35,7 @@ using System.Security;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Oracle")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Copyright © 2004, 2015, Oracle and/or its affiliates. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright © 2004, 2016, Oracle and/or its affiliates. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
--- Source/MySql.Data/Properties/VersionInfo.cs
+++ Source/MySql.Data/Properties/VersionInfo.cs
@@ -1,4 +1,4 @@
-// Copyright © 2004, 2015, Oracle and/or its affiliates. All rights reserved.
+// Copyright © 2004, 2016, Oracle and/or its affiliates. All rights reserved.
 //
 // MySQL Connector/NET is licensed under the terms of the GPLv2
 // <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
@@ -38,6 +38,6 @@ using System.Resources;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("6.9.8")]
-[assembly: AssemblyInformationalVersion("6.9.8")]
+[assembly: AssemblyVersion("7.0.2")]
+[assembly: AssemblyInformationalVersion("7.0.2")]
 [assembly: NeutralResourcesLanguage("en-US")]
