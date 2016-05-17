924b7a1176e1ebeadd9a6c281773459e07bf4e5c
Gabriela Martinez <gabry.martinez@oracle.com>
2016-05-17 13:00:48 -0500

    Renaming from MySql.Data.Entity to EntityFrameworkCore

---
+++ Tests/MySql.EntityFrameworkCore.Basic.Tests/DbContextClasses/TestsContexts.cs
@@ -0,0 +1,117 @@
+﻿// Copyright © 2016, Oracle and/or its affiliates. All rights reserved.
+//
+// MySQL Connector/NET is licensed under the terms of the GPLv2
+// <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
+// MySQL Connectors. There are special exceptions to the terms and 
+// conditions of the GPLv2 as it is applied to this software, see the 
+// FLOSS License Exception
+// <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
+//
+// This program is free software; you can redistribute it and/or modify 
+// it under the terms of the GNU General Public License as published 
+// by the Free Software Foundation; version 2 of the License.
+//
+// This program is distributed in the hope that it will be useful, but 
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License 
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along 
+// with this program; if not, write to the Free Software Foundation, Inc., 
+// 51 Franklin St, Fifth Floor, Boston, MA 02110-1301  USA
+
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using MySQL.Data.EntityFrameworkCore.Extensions;
+using System;
+
+namespace MySql.Data.EntityFrameworkCore.Tests.DbContextClasses
+{
+
+  public class NoConfigurationContext : DbContext
+  {
+    public DbSet<Blog> Blogs { get; set; }
+    //Empty configuration
+  }
+
+  public class SimpleContext : DbContext
+  {    
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    public SimpleContext(DbContextOptions options): base(options)
+    {      
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Blog>()
+              .HasOne(p => p.RecentPost)
+              .WithOne(i => i.Blog)
+              .HasForeignKey<Post>(b => b.PostId);
+
+
+      modelBuilder.Entity<Blog>()
+            .HasOne(p => p.Metadata)
+            .WithOne(i => i.Blog)
+            .HasForeignKey<BlogMetadata>(b => b.BlogId);
+
+    }
+
+  }
+
+
+  public class TestsContext : DbContext
+  {
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+    public DbSet<Read> Reads { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+
+    public DbSet<ReadTag> ReadTags { get; set; }
+
+    public TestsContext()
+    { }
+
+    public TestsContext(DbContextOptions options): base(options)
+    {
+    }   
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+       optionsBuilder.UseMySQL(MySQLTestStore.baseConnectionString + ";database=test;");
+    }
+
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Blog>()
+                .HasOne(p => p.RecentPost)
+                .WithOne(i => i.Blog)
+                .HasPrincipalKey<Blog>(p => new { p.BlogId, p.Url })
+                .HasForeignKey<Post>(b => new { b.BlogIdFK, b.Url });
+
+      modelBuilder.Entity<BlogMetadata>()
+               .HasOne(p => p.Blog)
+               .WithOne(i => i.Metadata)
+               .HasForeignKey<Blog>(b => b.BlogId);
+
+
+      modelBuilder.Entity<ReadTag>()
+               .HasKey(t => new { t.ReadId, t.TagId });
+
+      modelBuilder.Entity<ReadTag>()
+          .HasOne(pt => pt.Read)
+          .WithMany(p => p.ReadTags)
+          .HasForeignKey(pt => pt.ReadId)
+          .OnDelete(DeleteBehavior.Cascade);
+          
+      modelBuilder.Entity<ReadTag>()
+          .HasOne(pt => pt.Tag)
+          .WithMany(t => t.ReadTags)
+          .HasForeignKey(pt => pt.TagId);
+
+    }
+  }
+}
---
+++ Tests/MySql.EntityFrameworkCore.Migrations.Tests/MySqlMigrationsGeneratorTest.cs
@@ -0,0 +1,120 @@
+﻿
+
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage.Internal;
+using MySql.EntityFrameworkCore.Migrations.Tests.Utilities;
+using MySQL.Data.EntityFrameworkCore;
+using MySQL.Data.EntityFrameworkCore.Metadata;
+using MySQL.Data.EntityFrameworkCore.Migrations;
+using System.Diagnostics;
+using Xunit;
+
+namespace MySql.EntityFrameworkCore.Migrations.Tests
+{
+  public class MySqlMigrationsGeneratorTest : MySqlMigrationsGeneratorTestBase
+  {
+
+    protected override IMigrationsSqlGenerator SqlGenerator
+    {
+      get
+      {
+        var typeMapper = new MySQLTypeMapper();
+
+        return new MySQLMigrationsSqlGenerator(
+            new RelationalCommandBuilderFactory(
+                new FakeSensitiveDataLogger<RelationalCommandBuilderFactory>(),
+                new DiagnosticListener("FakeListener"),
+                typeMapper), new MySQLSqlGenerationHelper(), typeMapper,
+            new MySQLAnnotationProvider());
+      }
+    }
+
+    [Fact]
+    public override void CreateTableOperation()
+    {
+      base.CreateTableOperation();
+
+      Assert.Equal(
+          "CREATE TABLE `People` (" + EOL +
+          "    `Id` int NOT NULL AUTO_INCREMENT," + EOL +
+          "    `EmployerId` int," + EOL +
+          "    `SSN` char(11)," + EOL +
+          "    PRIMARY KEY (`Id`)," + EOL +
+          "    UNIQUE (`SSN`)," + EOL +
+          "    FOREIGN KEY (`EmployerId`) REFERENCES `Companies` (`Id`)" + EOL +
+          ");" + EOL,
+          Sql);
+    }
+
+    [Fact]
+    public override void AddColumnOperation_with_maxLength()
+    {
+      base.AddColumnOperation_with_maxLength();
+      Assert.Equal("ALTER TABLE `Person` ADD `Name` varchar(30);" + EOL, Sql);
+    }
+
+    [Fact]
+    public override void AddColumnOperationWithComputedValueSql()
+    {
+      base.AddColumnOperationWithComputedValueSql();
+      Assert.Equal("ALTER TABLE `People` ADD `DisplayName` varchar(50) AS  (CONCAT_WS(' ', LastName , FirstName));" + EOL, Sql);
+    }
+
+    [Fact]
+    public override void AddColumnOperationWithDefaultValueSql()
+    {
+      base.AddColumnOperationWithDefaultValueSql();
+      Assert.Equal("ALTER TABLE `People` ADD `Timestamp` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP;"  + EOL, Sql);
+    }
+
+    [Fact]
+    public override void AlterColumnOperation()
+    {
+      base.AlterColumnOperation();
+      Assert.Equal("ALTER TABLE Person MODIFY `Age` int NOT NULL DEFAULT 7;" + EOL, Sql);
+    }
+
+
+    [Fact]
+    public override void AlterColumnOperationWithoutType()
+    {
+      base.AlterColumnOperationWithoutType();
+      Assert.Equal("ALTER TABLE Person MODIFY `Age` int NOT NULL;" + EOL, Sql);
+    }
+
+    [Fact]
+    public override void RenameTableOperationInSchema()
+    {
+      base.RenameTableOperationInSchema();
+      Assert.Equal("ALTER TABLE t1 RENAME t2;" + EOL, Sql);            
+    }
+
+    [Fact]
+    public override void CreateUniqueIndexOperation()
+    {
+      base.CreateUniqueIndexOperation();
+      Assert.Equal("CREATE UNIQUE INDEX `IXPersonName` ON Person (`FirstName`, `LastName`);" + EOL, Sql);
+    }
+
+    [Fact]
+    public override void CreateNonUniqueIndexOperation()
+    {
+      base.CreateNonUniqueIndexOperation();
+      
+      Assert.Equal("CREATE INDEX `IXPersonName` ON Person (`Name`);" + EOL, Sql);
+    }
+    
+    [Fact(Skip = "Rename index is only supported in 5.7")]
+    public override void RenameIndexOperation()
+    {
+      base.RenameIndexOperation();
+      Assert.Equal("DROP INDEX IXPersonName ON Person; CREATE INDEX IXNombre;" + EOL, Sql);
+    }
+    
+    public override void DropIndexOperation()
+    {
+      base.DropIndexOperation();
+      Assert.Equal("DROP INDEX IXPersonName ON Person;" + EOL, Sql);
+    }
+  }
+}
