01420f1bea5ab727b44de7d61e686b8556bdb82d
Reggie Burnett <reggie.burnett@oracle.com>
2016-06-22 18:19:46 -0500

    Got many of the normal connection tests working on core

---
+++ Tests/MySql.Data.Tests/Framework/Net451/AuthTests.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySql.Data.MySqlClient.Tests.Framework.Net451
+{
+  public class AuthTests : TestBase
+  {
+    [Fact]
+    public void TestIntegratedSecurityNoPoolingWithoutUser()
+    {
+      TestIntegratedSecurityWithUser(null, false);
+    }
+
+    [Fact]
+    public void TestIntegratedSecurityPoolingWithoutUser()
+    {
+      TestIntegratedSecurityWithUser(null, true);
+    }
+
+    public void TestIntegratedSecurityWithUser(string user, bool pooling)
+    {
+      const string PluginName = "authentication_windows";
+      string UserName = "auth_windows";
+      if (user != null)
+        UserName = user;
+
+      // Check if server has windows authentication plugin is installed		
+      MySqlDataReader reader = ExecuteReaderAsRoot("show plugins");
+
+      bool haveWindowsAuthentication = false;
+      using (reader)
+      {
+        while (reader.Read())
+        {
+          string name = (string)reader["Name"];
+          if (name == PluginName)
+          {
+            haveWindowsAuthentication = true;
+            break;
+          }
+        }
+      }
+      if (!haveWindowsAuthentication)
+        return;
+
+      bool haveAuthWindowsUser = false;
+      string pluginName = null;
+      string authenticationString = "";
+
+      // Check if predefined proxy user exists
+      string sql = string.Format("select plugin, authentication_string from mysql.user where user='{0}'", UserName);
+      using (MySqlDataReader reader2 = ExecuteReaderAsRoot(sql))
+      {
+        if (reader2.Read())
+        {
+          haveAuthWindowsUser = true;
+          pluginName = (string)reader2["plugin"];
+          authenticationString =
+            (string)((reader2["authentication_string"] == DBNull.Value) ?
+            "" : reader2["authentication_string"]);
+        }
+      }
+
+      // Create mapping for current Windows user=>foo_user
+      String windowsUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+      windowsUser = windowsUser.Replace("\\", "\\\\");
+      string userMapping = windowsUser + "=foo_user";
+
+      if (!haveAuthWindowsUser)
+      {
+        executeAsRoot(
+          "CREATE USER " + UserName + " IDENTIFIED WITH " + PluginName + " as '" +
+           userMapping + "'");
+      }
+      else
+      {
+        // extend mapping string for current user
+        executeAsRoot(
+          "UPDATE mysql.user SET authentication_string='" + userMapping +
+          "," + authenticationString + "' where user='" + UserName + "'");
+      }
+      executeAsRoot("create user foo_user identified by 'pass'");
+      executeAsRoot("grant all privileges on *.* to 'foo_user'@'%'");
+      executeAsRoot("grant proxy on foo_user to " + UserName);
+
+
+      // Finally, use IntegratedSecurity=true for the newly created user
+      string connStr = st.GetConnectionString(true) + ";Integrated Security=SSPI";
+
+      MySqlConnectionStringBuilder sb =
+          new MySqlConnectionStringBuilder(connStr);
+      sb.UserID = user;
+      connStr = sb.ConnectionString;
+
+      /* If pooling is requested, we'll  run test twice, with connection reset in between */
+      if (pooling)
+      {
+        connStr += ";Connection Reset=true;Pooling=true";
+      }
+      int testIterations = pooling ? 2 : 1;
+
+      int threadId = -1;
+      for (int i = 0; i < testIterations; i++)
+      {
+        using (MySqlConnection c = new MySqlConnection(connStr))
+        {
+          c.Open();
+          threadId = c.ServerThread;
+          MySqlCommand command = new MySqlCommand("SELECT 1", c);
+          long ret = (long)command.ExecuteScalar();
+          Assert.Equal(ret, 1);
+
+          command.CommandText = "select user()";
+          string myUser = (string)command.ExecuteScalar();
+          // Check if proxy user is correct
+          Assert.True(myUser.StartsWith(UserName + "@"));
+
+          // check if mysql user is correct 
+          // (foo_user is mapped to current  OS user)
+          command.CommandText = "select current_user()";
+          string currentUser = (string)command.ExecuteScalar();
+          Assert.True(currentUser.StartsWith("foo_user@"));
+        }
+      }
+
+      if (pooling)
+      {
+        executeAsRoot("KILL " + threadId);
+      }
+    }
+
+    [Fact]
+    public void TestIntegratedSecurityNoPoolingWithUser()
+    {
+      TestIntegratedSecurityWithUser("myuser1", false);
+    }
+
+    [Fact]
+    public void TestIntegratedSecurityPoolingWithUser()
+    {
+      TestIntegratedSecurityWithUser("myuser1", true);
+    }
+
+    public void TestIntegratedSecurityWithoutProxy(string user, bool pooling)
+    {
+      const string PluginName = "authentication_windows";
+      string UserName = "auth_windows";
+      if (user != null)
+        UserName = user;
+
+      // Check if server has windows authentication plugin is installed			
+      MySqlCommand cmd = new MySqlCommand("show plugins", st.rootConn);
+
+      bool haveWindowsAuthentication = false;
+      using (MySqlDataReader r = cmd.ExecuteReader())
+      {
+        while (r.Read())
+        {
+          string name = (string)r["Name"];
+          if (name == PluginName)
+          {
+            haveWindowsAuthentication = true;
+            break;
+          }
+        }
+      }
+      if (!haveWindowsAuthentication)
+        return;
+
+      bool haveAuthWindowsUser = false;
+      string pluginName = null;
+      string authenticationString = "";
+
+      // Check if predefined proxy user exists
+      cmd.CommandText = string.Format(
+        "select plugin, authentication_string from mysql.user where user='{0}'",
+        UserName);
+      using (MySqlDataReader r = cmd.ExecuteReader())
+      {
+        if (r.Read())
+        {
+          haveAuthWindowsUser = true;
+          pluginName = (string)r["plugin"];
+          authenticationString =
+            (string)((r["authentication_string"] == DBNull.Value) ?
+            "" : r["authentication_string"]);
+        }
+      }
+
+      // Create mapping for current Windows user=>foo_user
+      String windowsUser = System.Security.Principal.WindowsIdentity.GetCurrent().Name;
+      windowsUser = windowsUser.Replace("\\", "\\\\");
+      string userMapping = "fergs, Administrators";
+
+      try
+      {
+        if (!haveAuthWindowsUser)
+        {
+          executeAsRoot(
+            "CREATE USER " + UserName + " IDENTIFIED WITH " + PluginName + " as '" +
+             userMapping + "'");
+        }
+        else
+        {
+          // extend mapping string for current user
+          executeAsRoot(
+            "UPDATE mysql.user SET authentication_string='" + userMapping +
+            "," + authenticationString + "' where user='" + UserName + "'");
+        }
+        executeAsRoot(string.Format("grant all privileges on *.* to '{0}'@'%'", UserName));
+
+        // Finally, use IntegratedSecurity=true for the newly created user
+        string connStr = st.GetConnectionString(true) + ";Integrated Security=SSPI";
+
+        MySqlConnectionStringBuilder sb =
+            new MySqlConnectionStringBuilder(connStr);
+        sb.UserID = user;
+        connStr = sb.ConnectionString;
+
+        /* If pooling is requested, we'll  run test twice, with connection reset in between */
+        if (pooling)
+        {
+          connStr += ";Connection Reset=true;Pooling=true";
+        }
+        int testIterations = pooling ? 2 : 1;
+
+        int threadId = -1;
+        for (int i = 0; i < testIterations; i++)
+        {
+          using (MySqlConnection c = new MySqlConnection(connStr))
+          {
+            c.Open();
+            threadId = c.ServerThread;
+            MySqlCommand command = new MySqlCommand("SELECT 1", c);
+            long ret = (long)command.ExecuteScalar();
+            Assert.Equal(ret, 1);
+
+            command.CommandText = "select user()";
+            string myUser = (string)command.ExecuteScalar();
+            // Check if proxy user is correct
+            Assert.True(myUser.StartsWith(UserName + "@"));
+
+            // check if mysql user is correct 
+            // (foo_user is mapped to current  OS user)
+            command.CommandText = "select current_user()";
+            string currentUser = (string)command.ExecuteScalar();
+            Assert.True(currentUser.StartsWith(UserName));
+          }
+        }
+
+        if (pooling)
+        {
+          executeAsRoot("KILL " + threadId);
+        }
+      }
+      finally
+      {
+        // Cleanup
+
+        // Drop test user
+        executeAsRoot(string.Format("drop user {0}", UserName));
+      }
+    }
+
+    [Fact]
+    public void TestWinAuthWithoutProxyNoUserNoPooling()
+    {
+      TestIntegratedSecurityWithoutProxy(null, false);
+    }
+
+    [Fact]
+    public void TestWinAuthWithoutProxyNoUserPooling()
+    {
+      TestIntegratedSecurityWithoutProxy("myuser1", true);
+    }
+
+    [Fact]
+    public void TestWinAuthWithoutProxyAndUser()
+    {
+      TestIntegratedSecurityWithoutProxy("myuser1", false);
+    }
+
+    [Fact]
+    public void TestWinAuthWithoutProxyAndUserPooling()
+    {
+      TestIntegratedSecurityWithoutProxy("myuser1", true);
+    }
+
+  }
+}
---
+++ Tests/MySql.Data.Tests/TestBase.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MySql.Data.MySqlClient.Tests
+{
+  public class TestBase : IDisposable
+  {
+
+    protected static string databaseName;
+    protected MySqlConnectionStringBuilder Settings;
+
+    static TestBase()
+    {
+      databaseName = "cnet_test_";
+    }
+
+    public TestBase()
+    {
+      // cleanup
+      executeAsRoot("DROP DATABASE IF EXISTS " + databaseName);
+      List<string> users = GetUserList(false);
+      foreach (var user in users)
+        executeAsRoot(String.Format("DROP USER IF EXISTS {0}", user));
+      executeAsRoot("FLUSH PRIVILEGES");
+
+      executeAsRoot("CREATE DATABASE " + databaseName);
+      executeAsRoot("CREATE USER 'test'@'localhost' IDENTIFIED BY 'test'");
+      executeAsRoot(String.Format("GRANT ALL ON *.* TO 'test'@'localhost'", databaseName));
+      executeAsRoot("FLUSH PRIVILEGES");
+      Settings = new MySqlConnectionStringBuilder();
+      Settings.Server = "localhost";
+      Settings.UserID = "test";
+      Settings.Password = "test";
+      Settings.Database = databaseName;
+      Settings.AllowUserVariables = true;
+      Settings.Pooling = false;
+      Settings.PersistSecurityInfo = true;
+    }
+
+    private List<string> GetUserList(bool includeRoot)
+    {
+      var list = new List<string>();
+      var reader = ExecuteReaderAsRoot("select user,host from mysql.user");
+      while (reader.Read())
+      {
+        string user = reader.GetString(0);
+        if (user == "root" && !includeRoot) continue;
+        list.Add(String.Format("'{0}'@'{1}'", reader.GetString(0), reader.GetString(1)));
+      }
+      return list;
+    }
+
+    protected MySqlConnection GetRoot()
+    {
+      return new MySqlConnection("server=localhost;port=3306;user id=root;database=mysql");
+    }
+    protected void executeAsRoot(string sql)
+    {
+      using (MySqlConnection root = GetRoot())
+      {
+        try
+        {
+          root.Open();
+          MySqlCommand cmd = new MySqlCommand(sql, root);
+          cmd.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+          string s = ex.Message;
+        }
+      }
+    }
+
+    protected MySqlDataReader ExecuteReaderAsRoot(string sql)
+    {
+      MySqlConnection root = GetRoot();
+      root.Open();
+      MySqlCommand cmd = new MySqlCommand(sql, root);
+      return cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+    }
+
+    protected void KillConnection(MySqlConnection c)
+    {
+      int threadId = c.ServerThread;
+      var root = GetRoot();
+      root.Open();
+      MySqlCommand cmd = new MySqlCommand("KILL " + threadId, root);
+      cmd.ExecuteNonQuery();
+
+      // the kill flag might need a little prodding to do its thing
+      try
+      {
+        cmd.CommandText = "SELECT 1";
+        cmd.Connection = c;
+        cmd.ExecuteNonQuery();
+      }
+      catch (Exception) { }
+
+      // now wait till the process dies
+      bool processStillAlive = false;
+      while (true)
+      {
+        MySqlCommand cmdProcess = new MySqlCommand("SHOW PROCESSLIST", root);
+        MySqlDataReader dr = cmdProcess.ExecuteReader();
+        while (dr.Read())
+        {
+          if (dr.GetInt32(0) == threadId) processStillAlive = true;
+        }
+        dr.Close();
+
+        if (!processStillAlive) break;
+        System.Threading.Thread.Sleep(500);
+      }
+    }
+
+    public void Dispose()
+    {
+    }
+  }
+}
