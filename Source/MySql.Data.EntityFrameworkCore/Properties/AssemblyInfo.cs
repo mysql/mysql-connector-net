042ef76712bd2e17d1783b80a0377f8247b95211
Gabriela Martinez <gabry.martinez@oracle.com>
2016-09-07 13:04:23 -0500

    Fix buildoptions for MySql.Data.EntityFrameworkCore

--- Source/MySql.Data.EntityFrameworkCore/Properties/AssemblyInfo.cs
+++ Source/MySql.Data.EntityFrameworkCore/Properties/AssemblyInfo.cs
@@ -46,8 +46,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("540e7b3c-bd0b-4980-96d1-5d140d303f7e")]
-//#if DEBUG
-//[assembly: AssemblyDelaySign(true)]
-//[assembly: AssemblyKeyFileAttribute(@"..\ConnectorNetPublicKey.snk")]
-//#endif
 [assembly: AssemblyKeyName("ConnectorNet")]
