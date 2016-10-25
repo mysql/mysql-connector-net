f9bb7ca9db6761315f8dbfe7cac2b7d3bc4528ce
Gabriela Martinez <gabry.martinez@oracle.com>
2016-10-25 16:10:47 -0500

    Removed X folder from mysql.data and MySqlX.Data.Tests project

--- Tests/MySqlX.Data.Tests/Properties/CreateUsers.sql
+++
@@ -1,9 +0,0 @@
-DROP USER IF EXISTS 'test'@'localhost';
-DROP USER IF EXISTS 'testNoPass'@'localhost';
-CREATE USER 'test'@'localhost' identified by 'test';
-GRANT ALL PRIVILEGES  ON *.*  TO 'test'@'localhost';
-CREATE USER 'testNoPass'@'localhost';
-FLUSH PRIVILEGES;
-
-
-
