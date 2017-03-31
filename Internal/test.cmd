4425530a0bfcb85cfcbc5653e685b34a0e4e0efe
Reggie Burnett <reggie.burnett@oracle.com>
2017-03-31 12:26:05 -0500

    add a dotnet restore cmd to the testing script to make sure the testing libs are restored.

--- Internal/test.cmd
+++ Internal/test.cmd
@@ -1,4 +1,5 @@
 cd MySql.Data\tests
+dotnet restore MySql.Data.Test.csproj
 copy certificates\*.* %MYSQL_DATADIR%\
 dotnet test MySql.Data.Tests.csproj -f net452 -c Debug
 dotnet test MySql.Data.Tests.csproj -f netcoreapp1.1 -c Debug
