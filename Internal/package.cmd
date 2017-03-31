236adfe012004957d467d906a610341baf61ab79
Reggie Burnett <reggie.burnett@oracle.com>
2017-03-31 10:21:08 -0500

    updating the test and package scripts

--- Internal/package.cmd
+++ Internal/package.cmd
@@ -1,3 +1,15 @@
+rem @echo off
+rem SET PROJECT_DIR=%cd%
+
+rem @echo on
+Nuget.exe install msbuildtasks -o packages
+Nuget.exe install MSBuild.Extension.Pack -o packages
+copy Internal\package.proj .
+
+msbuild package.proj /p:Configuration=%1
+
+IF "%1" == "commercial" EXIT 0
+
 REM ================== Make Nuget packages ======================================
 dotnet pack MySql.Data/src/MySql.Data.csproj -c Release
 dotnet pack MySql.Web/src/MySql.Web.csproj -c Release
