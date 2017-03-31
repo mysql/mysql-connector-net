a2d021407edadb63cf808f3fae53a39aaa807a76
Reggie Burnett <reggie.burnett@oracle.com>
2017-03-31 13:34:58 -0500

    fixed typo

--- Internal/test.cmd
+++ Internal/test.cmd
@@ -1,5 +1,5 @@
 cd MySql.Data\tests
-dotnet restore MySql.Data.Test.csproj
+dotnet restore MySql.Data.Tests.csproj
 copy certificates\*.* %MYSQL_DATADIR%\
 dotnet test MySql.Data.Tests.csproj -f net452 -c Debug
 dotnet test MySql.Data.Tests.csproj -f netcoreapp1.1 -c Debug
