eaf06baa401a40c21d5a18f8ea375afa61b2c037
Reggie Burnett <reggie.burnett@oracle.com>
2017-05-12 11:13:29 -0500

    we go back to delay signing all assemblies

--- EntityFrameworkCore/src/MySql.Data.EntityFrameworkCore/Properties/AssemblyInfo.cs
+++ EntityFrameworkCore/src/MySql.Data.EntityFrameworkCore/Properties/AssemblyInfo.cs
@@ -51,6 +51,3 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("540e7b3c-bd0b-4980-96d1-5d140d303f7e")]
 
-#if !DEBUG
-[assembly: AssemblyKeyName("ConnectorNet")]
-#endif
--- MySql.Web/tests/Properties/AssemblyInfo.cs
+++ MySql.Web/tests/Properties/AssemblyInfo.cs
@@ -40,10 +40,3 @@ using System.Runtime.InteropServices;
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("5ea74c78-c679-464f-99ba-ae1b0b54550b")]
-
-#if !DEBUG
-[assembly: AssemblyKeyName("ConnectorNet")]
-#endif
