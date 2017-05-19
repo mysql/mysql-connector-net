cc9fda30d7a878291df1b23468dbb141bf8b7581
Reggie Burnett <reggie.burnett@oracle.com>
2017-05-19 10:16:06 -0500

    made a few changes to hopefully fix dotnet pack step.

--- Internal/package.cmd
+++ Internal/package.cmd
@@ -4,10 +4,20 @@ rem SET PROJECT_DIR=%cd%
 rem @echo on
 IF "%1" == "gpl" (
   REM =============  Creating Nuget packages ===================================
+  dotnet restore MySql.Data/src/MySql.Data.csproj
   dotnet pack MySql.Data/src/MySql.Data.csproj -c Release -o ..\..\NugetPkgs\Package
+
+  dotnet restore MySql.Web/src/MySql.Web.csproj
   dotnet pack MySql.Web/src/MySql.Web.csproj -c Release -o ..\..\NugetPkgs\Package
-  dotnet pack EntityFrameworkCore/src/MySql.Data.EntityFrameworkCore/MySql.Data.EntityFrameworkCore.csproj -c Release -o ..\..\NugetPkgs\Package
-  dotnet pack EntityFrameworkCore/src/MySql.Data.EntityFrameworkCore.Design/MySql.Data.EntityFrameworkCore.Design.csproj -c Release -o ..\..\NugetPkgs\Package
+
+  dotnet restore EntityFrameworkCore/src/MySql.Data.EntityFrameworkCore/MySql.Data.EntityFrameworkCore.csproj
+  dotnet pack EntityFrameworkCore/src/MySql.Data.EntityFrameworkCore/MySql.Data.EntityFrameworkCore.csproj -c Release -o ..\..\..\NugetPkgs\Package
+
+  dotnet restore EntityFrameworkCore/src/MySql.Data.EntityFrameworkCore.Design/MySql.Data.EntityFrameworkCore.Design.csproj
+  dotnet pack EntityFrameworkCore/src/MySql.Data.EntityFrameworkCore.Design/MySql.Data.EntityFrameworkCore.Design.csproj -c Release -o ..\..\..\NugetPkgs\Package
+
+  dotnet restore EntityFramework6/src/MySql.Data.Entity.EF6.csproj
+  dotnet pack EntityFramework6/src/MySql.Data.Entity.EF6.csproj -c Release -o ..\..\NugetPkgs\Package
 )
 
 Nuget.exe install msbuildtasks -o packages
