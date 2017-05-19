344a9dcea0314785b9ae2ca8248cec38fc7a942f
Reggie Burnett <reggie.burnett@oracle.com>
2017-05-19 11:40:11 -0500

    Removing the unused files and changing name of test results files hoping to get the xunit report collector working on Jenkins

--- Internal/test.cmd
+++ Internal/test.cmd
@@ -5,38 +5,38 @@ REM =================== Test MySql.Data ========================================
 cd MySql.Data\tests
 dotnet restore 
 copy certificates\*.* %MYSQL_DATADIR%\
-dotnet xunit -framework net452 -parallel none -xml n452-test-results.xml
-dotnet xunit -framework netcoreapp1.1 -parallel none -xml netcore-test-results.xml
+dotnet xunit -framework net452 -parallel none -xml mysql-data-test-results.xml
+dotnet xunit -framework netcoreapp1.1 -parallel none -xml mysql-data-core-test-results.xml
 cd ../..
 
 REM =================== Test MySql.Web ==================================================
 cd MySql.Web\tests
 dotnet restore 
-dotnet xunit -framework net452 -parallel none -xml n452-test-results.xml
+dotnet xunit -framework net452 -parallel none -xml mysql-web-test-results.xml
 cd ../..
 
 REM =================== Test EF Core =====================================================
 cd EntityFrameworkCore/tests/MySql.EntityFrameworkCore.Basic.Tests
 dotnet restore
-dotnet xunit -framework net452 -xml net452-test-results.xml
-dotnet xunit -framework netcoreapp1.1 -xml netcore-test-results.xml
+dotnet xunit -framework net452 -xml mysql-efcore-test-results.xml
+dotnet xunit -framework netcoreapp1.1 -xml mysql-efcore-core-test-results.xml
 
 cd ../MySql.EntityFrameworkCore.Design.Tests/
 dotnet restore
-dotnet xunit -framework net452 -xml net452-test-results.xml
-dotnet xunit -framework netcoreapp1.1 -xml netcore-test-results.xml
+dotnet xunit -framework net452 -xml mysql-efcoredesign-test-results.xml
+dotnet xunit -framework netcoreapp1.1 -xml mysq-efcoredesign-core-test-results.xml
 
 cd ../MySql.EntityFrameworkCore.Migrations.Tests
 dotnet restore
-dotnet xunit -framework net452 -xml net452-test-results.xml
-dotnet xunit -framework netcoreapp1.1 -xml netcore-test-results.xml
+dotnet xunit -framework net452 -xml mysql-efcoremigrations-test-results.xml
+dotnet xunit -framework netcoreapp1.1 -xml mysql-efcoremigrations-core-test-results.xml
 
 cd ../../..
 
 REM =================== Test EF 6 =====================================================
 cd EntityFramework6/tests/MySql.EntityFramework6.Basic.Tests
 dotnet restore
-dotnet xunit -xml net452-test-results.xml
+dotnet xunit -xml mysql-ef6-test-results.xml
 
 
 
--- Internal/test.sh
+++ Internal/test.sh
@@ -7,20 +7,20 @@ fi
 echo '=========== Testing MySql.Data ====================='
 cd MySQL.Data/tests
 dotnet restore 
-dotnet xunit -framework netcoreapp1.1 -parallel none -xml netcore-test-results.xml
+dotnet xunit -framework netcoreapp1.1 -parallel none -xml mysql-data-core-test-results.xml
 cd ../..
 
 echo '============== Testing EF Core ======================'
 cd EntityFrameworkCore/tests/MySql.EntityFrameworkCore.Basic.Tests
 dotnet restore
-dotnet xunit -framework netcoreapp1.1 -xml netcore-test-results.xml
+dotnet xunit -framework netcoreapp1.1 -xml mysql-efcore-core-test-results.xml
 
 cd ../MySql.EntityFrameworkCore.Design.Tests/
 dotnet restore
-dotnet xunit -framework netcoreapp1.1 -xml netcore-test-results.xml
+dotnet xunit -framework netcoreapp1.1 -xml mysql-efcoredesign-core-test-results.xml
 
 cd ../MySql.EntityFrameworkCore.Migrations.Tests
 dotnet restore
-dotnet xunit -framework netcoreapp1.1 -xml netcore-test-results.xml
+dotnet xunit -framework netcoreapp1.1 -xml mysql-efcoremigrations-core-test-results.xml
 
 cd ../../..
