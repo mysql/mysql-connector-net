1396032df5507ff9c40430957a47ce9e2b94d2df
Roberto Garcia <robertoe.garcia@oracle.com>
2017-05-31 14:58:21 -0500

    Fixes for unit tests in MySql Server 5.6 and 5.7

--- Internal/test.cmd
+++ Internal/test.cmd
@@ -3,6 +3,7 @@ IF NOT "%1" == ""  SET MYSQL_PORT=%1
 
 REM =================== Test MySql.Data ==================================================
 cd MySql.Data\tests
+dotnet clean
 dotnet restore 
 copy certificates\*.* %MYSQL_DATADIR%\
 dotnet build MySql.Data.Tests.csproj -c Debug
@@ -14,6 +15,7 @@ cd ../..
 
 REM =================== Test MySql.Web ==================================================
 cd MySql.Web\tests
+dotnet clean
 dotnet restore 
 dotnet build MySql.Web.Tests.csproj -c Debug
 sn.exe -Rca  bin\debug\net452\MySql.Web.dll ConnectorNet
@@ -23,6 +25,7 @@ cd ../..
 
 REM =================== Test EF Core =====================================================
 cd EntityFrameworkCore/tests/MySql.EntityFrameworkCore.Basic.Tests
+dotnet clean
 dotnet restore
 dotnet build MySql.EntityFrameworkCore.Basic.Tests.csproj -c Debug
 sn.exe -Rca  bin\debug\net452\MySql.EntityFrameworkCore.Basic.Tests.dll ConnectorNet
@@ -30,6 +33,7 @@ dotnet xunit -framework net452 -xml mysql-efcore-test-results.xml
 dotnet xunit -framework netcoreapp1.1 -xml mysql-efcore-core-test-results.xml
 
 cd ../MySql.EntityFrameworkCore.Design.Tests/
+dotnet clean
 dotnet restore
 dotnet build MySql.EntityFrameworkCore.Design.Tests.csproj -c Debug
 sn.exe -Rca  bin\debug\net452\MySql.EntityFrameworkCore.Design.Tests.dll ConnectorNet
@@ -37,6 +41,7 @@ dotnet xunit -framework net452 -xml mysql-efcoredesign-test-results.xml
 dotnet xunit -framework netcoreapp1.1 -xml mysq-efcoredesign-core-test-results.xml
 
 cd ../MySql.EntityFrameworkCore.Migrations.Tests
+dotnet clean
 dotnet restore
 dotnet build MySql.EntityFrameworkCore.Migrations.Tests.csproj -c Debug
 sn.exe -Rca  bin\debug\net452\MySql.EntityFrameworkCore.Migrations.Tests.dll ConnectorNet
@@ -47,6 +52,7 @@ cd ../../..
 
 REM =================== Test EF 6 =====================================================
 cd EntityFramework6/tests/MySql.EntityFramework6.Basic.Tests
+dotnet clean
 dotnet restore
 dotnet build MySql.EntityFramework6.Basic.Tests.csproj -c Debug
 sn.exe -Rca  bin\debug\net452\MySql.EntityFramework6.Basic.Tests.dll ConnectorNet
