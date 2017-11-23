c9de0f43fd67fd374d14e76fbb1bb36a8b93ada6
Roberto Garcia <robertoe.garcia@oracle.com>
2017-11-23 17:23:39 -0600

    Bug #20393654 SOCKETS STAY IN CLOSE_WAIT WHEN USING SSL
    Changed leaveInnerStreamOpen to false in the SslStream to close the socket when disposing.

--- Source/MySql.Data/NativeDriver.cs
+++ Source/MySql.Data/NativeDriver.cs
@@ -1,4 +1,4 @@
-// Copyright © 2004, 2016, Oracle and/or its affiliates. All rights reserved.
+// Copyright © 2004, 2017 Oracle and/or its affiliates. All rights reserved.
 //
 // MySQL Connector/NET is licensed under the terms of the GPLv2
 // <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
@@ -380,7 +380,7 @@ namespace MySql.Data.MySqlClient
     {
       RemoteCertificateValidationCallback sslValidateCallback =
           new RemoteCertificateValidationCallback(ServerCheckValidation);
-      SslStream ss = new SslStream(baseStream, true, sslValidateCallback, null);
+      SslStream ss = new SslStream(baseStream, false, sslValidateCallback, null);
       X509CertificateCollection certs = GetClientCertificates();
       SslProtocols sslProtocols = SslProtocols.Tls;
 #if NET_45_OR_GREATER
