bdea47868ce9d62b7c057fcd4516ccb75d861fe1
Roberto Garcia <robertoe.garcia@oracle.com>
2018-01-10 15:18:17 -0600

    Bump up version to 6.10.6

--- EntityFrameworkCore/src/MySql.Data.EntityFrameworkCore/Frameworks/efcore10/Properties/AssemblyInfo.cs
+++ EntityFrameworkCore/src/MySql.Data.EntityFrameworkCore/Frameworks/efcore10/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2017 Oracle and/or its affiliates. All rights reserved.
+﻿// Copyright © 2017, 2018 Oracle and/or its affiliates. All rights reserved.
 //
 // MySQL Connector/NET is licensed under the terms of the GPLv2
 // <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
@@ -24,5 +24,5 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 
 [assembly: DesignTimeProviderServices(
   typeName: "MySql.Data.EntityFrameworkCore.Design.Internal.MySQLDesignTimeServices",
-  assemblyName: "MySql.Data.EntityFrameworkCore.Design, Version=6.10.5.0, Culture=neutral, PublicKeyToken=c5687fc88969c44d",
+  assemblyName: "MySql.Data.EntityFrameworkCore.Design, Version=6.10.6.0, Culture=neutral, PublicKeyToken=c5687fc88969c44d",
   packageName: "MySql.Data.EntityFrameworkCore.Design")]
