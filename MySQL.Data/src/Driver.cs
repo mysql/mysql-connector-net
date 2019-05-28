0c79932655cdd545afb15947d36ebcbe909426d4
Daniel Valdez <daniel.valdez@oracle.com>
2019-05-28 16:46:40 -0500

    Fix Bug #28928543 - CONNECTOR/NET DOESN'T NEED TO RUN SHOW VARIABLES

--- MySQL.Data/src/Driver.cs
+++ MySQL.Data/src/Driver.cs
@@ -1,4 +1,4 @@
-// Copyright © 2004, 2017 Oracle and/or its affiliates. All rights reserved.
+// Copyright (c) 2004, 2019, Oracle and/or its affiliates. All rights reserved.
 //
 // MySQL Connector/NET is licensed under the terms of the GPLv2
 // <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most 
@@ -284,16 +284,20 @@ namespace MySql.Data.MySqlClient
     {
       // load server properties
       Dictionary<string, string> hash = new Dictionary<string, string>();
-      MySqlCommand cmd = new MySqlCommand("SHOW VARIABLES", connection);
+      MySqlCommand cmd = new MySqlCommand(@"SELECT @@max_allowed_packet, @@character_set_client, 
+        @@character_set_connection, @@license, @@sql_mode, @@lower_case_table_names;", connection);
       try
       {
         using (MySqlDataReader reader = cmd.ExecuteReader())
         {
           while (reader.Read())
           {
-            string key = reader.GetString(0);
-            string value = reader.GetString(1);
-            hash[key] = value;
+            for (int i = 0; i < reader.FieldCount-1; i++)
+            {
+              string key = reader.GetName(i).Remove(0,2);
+              string value = reader[i].ToString();
+              hash[key] = value;
+            }
           }
         }
         // Get time zone offset as numerical value
