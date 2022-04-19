308c58f0c80a1aa1d92691f3e3c7dea6e387bd20
Daniel Valdez <daniel.valdez@oracle.com>
2022-04-19 13:47:44 -0500

    Bump up version to 8.0.30
    Change-Id: I729e70af5a3f1b2f37cf1f82358ca01d8f85f771

--- MySQL.Data/src/Properties/AssemblyInfo.cs
+++ MySQL.Data/src/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright © 2004, 2019, Oracle and/or its affiliates. All rights reserved.
+﻿// Copyright (c) 2004, 2022, Oracle and/or its affiliates.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License, version 2.0, as
@@ -40,7 +40,7 @@ using System.Security;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Oracle")]
 [assembly: AssemblyProduct("MySql.Data.Core")]
-[assembly: AssemblyCopyright("Copyright © 2016, 2020, Oracle and/or its affiliates. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 2016, 2022, Oracle and/or its affiliates.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: SecurityRules(SecurityRuleSet.Level1)]
