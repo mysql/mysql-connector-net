71d8b45a4e41faacef226eb5c148d49112b9008e
Omar Chavez <omar.c.chavez@oracle.com>
2023-03-12 19:11:24 -0600

    Fixed MySql.Data tests.
    Change-Id: Ida18976a91f56ccf9b100bb1eeb766322aab1531

--- MySQL.Data/tests/MySql.Data.Tests/CmdTests.cs
+++ MySQL.Data/tests/MySql.Data.Tests/CmdTests.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2013, 2022, Oracle and/or its affiliates.
+﻿// Copyright (c) 2013, 2023, Oracle and/or its affiliates.
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License, version 2.0, as
@@ -971,7 +971,7 @@ namespace MySql.Data.MySqlClient.Tests
       ExecuteSQL(@"CREATE TABLE Test (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, text CHAR(100))");
 
       using var cmd = Connection.CreateCommand();
-      cmd.CommandText = @"INSERT INTO test (text) VALUES ('test1'); INSERT INTO test (text) VALUES ('test2');";
+      cmd.CommandText = @"INSERT INTO Test (text) VALUES ('test1'); INSERT INTO Test (text) VALUES ('test2');";
 
       Assert.AreEqual(2, cmd.ExecuteNonQuery());
       Assert.AreEqual(2, cmd.LastInsertedId);
