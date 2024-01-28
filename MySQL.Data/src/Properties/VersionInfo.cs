6be9b86383701f4d6306590ec33df6475b5d81cd
Omar Chavez <omar.c.chavez@oracle.com>
2024-01-28 16:32:00 -0700

    Bump up version to 8.4.0
    Change-Id: Ic8490e909f79365b8147adc96f94e34ff24bb8c5

--- MySQL.Data/src/Properties/VersionInfo.cs
+++ MySQL.Data/src/Properties/VersionInfo.cs
@@ -40,6 +40,6 @@ using System.Resources;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("8.3.0")]
-[assembly: AssemblyInformationalVersion("8.3.0")]
+[assembly: AssemblyVersion("8.4.0")]
+[assembly: AssemblyInformationalVersion("8.4.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
