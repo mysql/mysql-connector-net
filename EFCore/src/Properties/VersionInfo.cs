208cb75c638c87a952bce304f53f2c5911f6cbbc
Filipe Silva <filipe.silva@oracle.com>
2025-10-22 17:00:15 +0100

    Bump Up version to 9.6.0
    Change-Id: I40cf6169c89ba4f96992b1f5256d8e778aca743f

--- EFCore/src/Properties/VersionInfo.cs
+++ EFCore/src/Properties/VersionInfo.cs
@@ -41,18 +41,18 @@ using System.Resources;
 // by using the '*' as shown below:
 
 #if NET8_0
-[assembly: AssemblyVersion("9.5.0")]
-[assembly: AssemblyInformationalVersion("8.0.20.0+MySQL9.5.0")]
-[assembly: AssemblyFileVersion("9.5.0.0")]
+[assembly: AssemblyVersion("9.6.0")]
+[assembly: AssemblyInformationalVersion("8.0.20.0+MySQL9.6.0")]
+[assembly: AssemblyFileVersion("9.6.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 #elif NET9_0
-[assembly: AssemblyVersion("9.5.0")]
-[assembly: AssemblyInformationalVersion("9.0.6.0+MySQL9.5.0")]
-[assembly: AssemblyFileVersion("9.5.0.0")]
+[assembly: AssemblyVersion("9.6.0")]
+[assembly: AssemblyInformationalVersion("9.0.6.0+MySQL9.6.0")]
+[assembly: AssemblyFileVersion("9.6.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 #elif NET10_0
-[assembly: AssemblyVersion("9.5.0")]
-[assembly: AssemblyInformationalVersion("10.0.0.0-rc+MySQL9.5.0")]
-[assembly: AssemblyFileVersion("9.5.0.0")]
+[assembly: AssemblyVersion("9.6.0")]
+[assembly: AssemblyInformationalVersion("10.0.0.0-rc+MySQL9.6.0")]
+[assembly: AssemblyFileVersion("9.6.0.0")]
 [assembly: NeutralResourcesLanguage("en-US")]
 #endif
